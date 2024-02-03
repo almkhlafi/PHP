7a79cbfe2817bfc814df8fbc3aa6f4989fb00a8a
Abdulrahman Almkhlafi <40529896+almkhlafi@users.noreply.github.com>
2024-02-04 01:46:02 +0200

    Revert "Revert "Update the Main Branch""

--- cookies.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!isset($_COOKIE['visits']))
- $_COOKIE['visits'] = 10;
-$_COOKIE['visits'] = $_COOKIE['visits'] + 1;
-setcookie('Pizza', $_COOKIE['visits'], time()+3600*24*365);
-?>
--- templates/header.php
+++ templates/header.php
@@ -12,14 +12,26 @@
 .brand-text{
     color:#cbb09c !important;
 }
+.form{
+    max-width:460px;
+    margin:20px auto;
+    padding:20px;
+}
+.pizza{
+    width:100px;
+    margin:-40px auto 13px;
+    display":block;
+    position: relative;
+    top: -30px;;
+}
     </style>
 </head>
-<body class="gray lighten-4">
+<body class="grey lighten-4">
     <nav class="wite z-depth-0">
         <div class="container">
-        <a herf="#" cass="brand-logo brand-text">Pizza </a>
+        <a href="orders.php" cass="brand-logo brand-text">Pizza </a>
         <ul id="nav-mobile" class="right hide-on-small-and-down"> 
-         <li><a herf="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+         <li><a href="addOrder.php" class="btn brand z-depth-0">Add a Pizza</a></li>
 </ul>
     </div>
 </nav>
