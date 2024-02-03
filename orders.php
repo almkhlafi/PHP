c15feb121e330a04c6b602d00a57bae63a8a998e
almkhlafi <azaymabood2015@gmail.com>
2024-02-03 13:52:34 +0200

    update last commit

--- orders.php
+++ orders.php
@@ -20,29 +20,37 @@ $conn->close();
 
 ?>
 
-
-
 <h4 class="center grey-text">Pizzas!</h4>
-    <div class="container">
-        <div class="row">
-            <?php foreach($pizzas as $pizza){ ?>
-            <div class="col s6 md3">
-                <div class="card z-depth-0">
-                    <div class="card-content center">
-                        <h6><?php echo htmlspecialchars($pizza['title']);?></h6>
-                            <ul>
-                            <?php foreach(explode(',',$pizzas[0]['ingredients']) as $ing){?>
-                             
-                             <li><?php echo htmlspecialchars($ing) ?><li>   
-                
-                        <?php } ?> 
-                    </ul>
-                        </div>
+<div class="container">
+    <div class="row">
+        <?php 
+        
+            foreach($pizzas as $pizza): ?>
+        <div class="col s6 md3">
+            <div class="card z-depth-0">
+                <div class="card-content center">
+                    <h6><?php echo htmlspecialchars($pizza['title']);?></h6>
+                    <ul>
+
+                        <?php foreach(explode(',',$pizza['ingredients']) as $ing):?>
+
+                        <li><?php echo htmlspecialchars($ing) ?>
+                            <li>
+                                <?php endforeach; ?>
+                        </ul>
+                    </div>
                     <div class="card-action right-align">
                         <a class="brand-text" herf="#">more into</a>
                     </div>
                 </div>
             </div>
-            <?php }?>
+            <?php endforeach;?>
+            <!-- more readable when for closing brackets-->
         </div>
-    </div>  <?php include 'templates/footer.php'; ?>
+        <?php if(count($pizzas)>0){?> 
+                            <p style="background-color:lightgray;">There are :<?php echo count($pizzas); ?></p>
+                        <?php }else{?>
+                            <p style="background-color:lightgray;">No Pizzz</p>
+                            <?php }?>
+    </div>
+    <?php include 'templates/footer.php'; ?>
